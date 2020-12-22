689ca2434f23dd2e143f6bca1be485d353536773
VARGASOFFICIAL <76422431+VARGASOFFICIAL@users.noreply.github.com>
2020-12-22 17:35:24 +0700

    Add files via upload

---
+++ libur/info.js
@@ -0,0 +1,38 @@
+exports.info = (id, XPTN, corohelp, tampilTanggal, tampilWaktu, instagram, nomer, aktif, groupwa, youtube) => {
+	return `*MENU ${XPTN}*
+  
+❉──────────❉
+  Hi. *${id.split("@s.whatsapp.net")[0]}* 👋️
+❉──────────❉
+
+         ───
+📆 *${tampilTanggal}*
+⏱️ *${tampilWaktu}*
+📢 Bot Aktif ; *${aktif}*
+         ───
+         
+╔════════════════════
+║ *About ${XPTN}*
+╠════════════════════
+║├≽️⚜ *🔰AUTHOR🔰*: _VARGAS_250_
+║├≽️🛡️ *DESIGNER*: _VARGAS_
+║├≽️🛡️ *YOUTUBE*: _VARGAS OFFICIAL_
+║├≽️🛡️ *saweria*: -
+╠════════════════════
+╠════════════════════
+║  *${XPTN}*
+╠════════════════════
+║╭──❉ *SOSMED ADMIN* ❉──
+║│1. *Group WhatsApp*
+║│ _${groupwa}_
+║│2. *YouTube <subscribe>*
+║│ _${youtube}_
+║│3. *Instagram <Follow>*
+║│ _${instagram}_
+║│4. *Creator ${XPTN}*
+║│ _${nomer}_
+║╰───────────
+╠════════════════════
+║ *MADE BY VARGAS_250*
+╚════════════════════`
+}
